3ce0ad41b6c7efc815413facdad23deecef374be
oksanabolbat <oksana.bolbat@gmail.com>
2023-05-12 21:10:35 +0300

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import Footer from './components/Footer';
 import MainView from './components/MainView';
 function App() {
   return (
     <div
-      className="container bg-light py-5 px-3 my-0 mx-auto rounded"
+      className="container bg-light pt-5 pb-2 px-3 my-0 mx-auto rounded"
       style={{ maxWidth: '800px' }}
     >
       <h1 className="mb-5">Weather Forecast</h1>
       <MainView />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="small opacity-50 text-center">
+      This project was coded by Oksana Bolbat and is open-sourced on{' '}
+      <a href="https://github.com/oksanabolbat/weather-ts">GitHub</a> and hosted
+      on <a href="https://www.netlify.com/">Netlify</a>
+    </footer>
+  );
 };
 
 export default Footer;
